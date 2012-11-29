a6b9fe46bdedd84509e38e31046106746309d42b
Oleg Moskalenko <mom040267@gmail.com>
2012-11-29 09:05:25 +0000

    Some cleaning

---
+++ scripts.examples/secure_tls_client.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib/
+
+testapps/bin/uclient -t -S -i etc/test_client_cert.pem -k etc/test_client_pkey.pem -n 1000 -m 10 -l 170 -e ::1 -g -u ninefingers -w youhavetoberealistic 127.0.0.1
+
