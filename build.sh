268139b80c49ec5069c57f202eda485ab0231185
Oleg Moskalenko <mom040267@gmail.com>
2012-12-10 04:30:08 +0000

    Build system improved.

--- build.sh
+++ build.sh
@@ -1,71 +1,132 @@
 #!/bin/sh
 
-SYSTEM=`uname`
+testlib() {
+    ${CC} ${TMPCPROGC} -o ${TMPCPROGB} ${OSLIBS} -l${1} 2>/dev/null
+    ER=$?
+    if ! [ ${ER} -eq 0 ] ; then
+	echo "Do not use -l${1}"
+	return 0
+    else
+	OSLIBS="${OSLIBS} -l${1}"
+	return 1
+    fi
+}
 
-GNUOSCFLAGS="-Wall -Wextra -Wformat-security -Wnested-externs -Wstrict-prototypes  -Wmissing-prototypes -Wpointer-arith -Wcast-qual -Wredundant-decls"
-
-#GNUOSCFLAGS="-Werror -Winline ${GNUOSCFLAGS}"
-
+#########################
+# To be set:
 OSCFLAGS=
+OSLIBS="-L/usr/local/lib/event2/ -L/usr/local/lib/ -Llib"
+#########################
+
+TMPDIR="."
+
+if [ -d /var/tmp ] ; then
+  TMPDIR="/var/tmp"
+elif [ -d /tmp ] ; then
+  TMPDIR=/tmp
+fi
+
+echo Use TMP dir ${TMPDIR}
+
+TMPCPROG=test
+TMPCPROGC=${TMPDIR}/${TMPCPROG}.c
+TMPCPROGB=${TMPDIR}/${TMPCPROG}
+
+cat > ${TMPCPROGC} <<!
+int main() {
+    return 0;
+}
+!
 
 if [ -z "${CC}" ] ; then
     CC=cc
+fi
+
+echo "Compiler: ${CC}"
+
+${CC} ${TMPCPROGC} -o ${TMPCPROGB}
+ER=$?
+if ! [ ${ER} -eq 0 ] ; then
+    echo "ERROR: cannot use compiler ${CC} properly"
+    exit
+fi
+
+GNUOSCFLAGS="-Wall -Wextra -Wformat-security -Wnested-externs -Wstrict-prototypes  -Wmissing-prototypes -Wpointer-arith -Wcast-qual -Wredundant-decls"
+
+${CC} ${GNUOSCFLAGS} ${TMPCPROGC} -o ${TMPCPROGB} 2>/dev/null
+ER=$?
+if ! [ ${ER} -eq 0 ] ; then
+    echo "Not an ordinary GNU or Clang compiler"
 else
-    if [ "${CC}" = "gcc" ] ; then
-	OSCFLAGS="${GNUOSCFLAGS}"
-    else
-	if [ "${CC}" = "clang" ] ; then
-	    OSCFLAGS="${GNUOSCFLAGS}"
-	fi
-    fi
+    OSCFLAGS="${OSCFLAGS} ${GNUOSCFLAGS}"
 fi
 
-if [ -z "${OSCFLAGS}" ] ; then  
-    COMPSIGN=`${CC} --version 2>/dev/null | grep -i gcc | cut -b 1`
-    RETRES=$?
-    if [ ${RETRES} -eq 0 ] ; then
-	if [ ${COMPSIGN} ] ; then
-	    OSCFLAGS="${GNUOSCFLAGS}"
-	fi
-    fi
+testlib socket
+testlib nsl
+testlib dl
+testlib pthread
+
+testlib ssl
+ER=$?
+if ! [ ${ER} -eq 0 ] ; then
+    echo "SSL lib installed."
+else
+    echo "ERROR: OpenSSL development libraries are not installed properly in /usr/local."
+    echo "Abort."
+    exit
 fi
 
-if [ -z "${OSCFLAGS}" ] ; then  
-    COMPSIGN=`${CC} --version 2>/dev/null | grep -i clang | cut -b 1`
-    RETRES=$?
-    if [ ${RETRES} -eq 0 ] ; then
-	if [ ${COMPSIGN} ] ; then
-	    OSCFLAGS="${GNUOSCFLAGS}"
-	fi
-    fi
+testlib crypto
+ER=$?
+if ! [ ${ER} -eq 0 ] ; then
+    echo "Crypto SSL lib installed."
+else
+    echo "ERROR: OpenSSL development libraries are not installed properly in /usr/local."
+    echo "Abort."
+    exit
 fi
 
-if [ -z "${OSCFLAGS}" ] ; then  
-    COMPSIGN=`${CC} --version 2>/dev/null | grep -i "Free Software Foundation" | cut -b 1`
-    RETRES=$?
-    if [ ${RETRES} -eq 0 ] ; then
-	if [ ${COMPSIGN} ] ; then
-	    OSCFLAGS="${GNUOSCFLAGS}"
-	fi
-    fi
+testlib event
+ER=$?
+if ! [ ${ER} -eq 0 ] ; then
+    echo "Libevent2 installed."
+else
+    echo "ERROR: Libevent2 development libraries are not installed properly in /usr/local."
+    echo "See the INSTALL file."
+    echo "Abort."
+    exit
 fi
+
+testlib event_pthreads
+ER=$?
+if ! [ ${ER} -eq 0 ] ; then
+    echo "Libevent2 pthreads installed."
+else
+    echo "ERROR: Libevent2 development libraries are not compiled with threads support."
+    echo "See the INSTALL file."
+    echo "Abort."
+    exit
+fi
+
+testlib event_openssl
+ER=$?
+if ! [ ${ER} -eq 0 ] ; then
+    echo "Libevent2-openssl installed."
+else
+    echo "ERROR: Libevent2 development libraries are not compiled with OpenSSL support."
+    echo "See the INSTALL file."
+    echo "Abort."
+    exit
+fi
+
+SYSTEM=`uname`
 
 if [ "${SYSTEM}" = "SunOS" ] ; then
 # Solaris
-    OSLIBS="-lsocket -lnsl -ldl"
     OSCFLAGS=${OSCFLAGS}" -D__EXTENSIONS__ -D_XOPEN_SOURCE=500"
 fi
 
-ISLINUX=`echo ${SYSTEM} | grep -i linux | cut -b 1`
-if [ ${ISLINUX} ] ; then
-#Linux
-  OSLIBS="-ldl"
-fi
+echo make OSLIBS="${OSLIBS}" OSCFLAGS="${OSCFLAGS}" $@
 
-ISBSD=`echo ${SYSTEM} | grep -i bsd | cut -b 1`
-if [ ${ISBSD} ] ; then
-#BSD
-  OSLIBS=
-fi
+make OSLIBS="${OSLIBS}" OSCFLAGS="${OSCFLAGS}" $@ -f Makefile.all
 
-make OSLIBS="${OSLIBS}" OSCFLAGS="${OSCFLAGS}" $@
