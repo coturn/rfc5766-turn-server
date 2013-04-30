b715d0fe72e33ed98cfd4510c5410f5530c3827a
Oleg Moskalenko <mom040267@gmail.com>
2013-04-30 08:50:41 +0000

    Changes by Lintian

--- make-man.sh
+++ make-man.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
+rm -rf man/man1/*
+
 txt2man -t TURN -I turnserver -I turnadmin -I turnutils -I turnutils_uclient -I turnutils_stunclient -I turnutils_rfc5769check -I turnutils_peer -B "TURN Server" README.turnserver > man/man1/turnserver.1
 
 txt2man -t TURN -I turnserver -I turnadmin -I turnutils -I turnutils_uclient -I turnutils_stunclient -I turnutils_rfc5769check -I turnutils_peer -B "TURN Server" README.turnadmin > man/man1/turnadmin.1
 
 txt2man -t TURN -I turnserver -I turnadmin -I turnutils -I turnutils_uclient -I turnutils_stunclient -I turnutils_rfc5769check -I turnutils_peer -B "TURN Server" README.turnutils > man/man1/turnutils.1
+
+cd man/man1; ln -s turnutils.1 turnutils_uclient.1;cd ../..
+cd man/man1; ln -s turnutils.1 turnutils_peer.1;cd ../..
+cd man/man1; ln -s turnutils.1 turnutils_rfc5769check.1;cd ../..
+cd man/man1; ln -s turnutils.1 turnutils_stunclient.1;cd ../..
+
+cd man/man1; ln -s turnutils.1 uclient.1;cd ../..
+cd man/man1; ln -s turnutils.1 peer.1;cd ../..
+cd man/man1; ln -s turnutils.1 rfc5769check.1;cd ../..
+cd man/man1; ln -s turnutils.1 stunclient.1;cd ../..
