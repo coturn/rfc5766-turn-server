8cc7262810f542161953b1ff1843469d266c1cc9
Oleg Moskalenko <mom040267@gmail.com>
2013-07-22 02:52:52 +0000

    A better fix for Issue 45

--- src/apps/relay/dtls_listener.c
+++ src/apps/relay/dtls_listener.c
@@ -417,11 +417,12 @@ static int listen_client_connection(dtls_listener_relay_server_type* server, new
 
 	int rc = dtls_listen(server->verbose,(*ndc)->info.ssl,&client_addr);
 
-	BIO_set_fd(SSL_get_rbio((*ndc)->info.ssl), (*ndc)->info.fd, BIO_NOCLOSE);
-
 	if(server->verbose) TURN_LOG_FUNC(TURN_LOG_LEVEL_INFO,"Listen: rc=%d\n",rc);
 
 	if(rc<0) return -1;
+
+	BIO_set_fd(SSL_get_rbio((*ndc)->info.ssl), (*ndc)->info.fd, BIO_NOCLOSE);
+
 	if(!rc) return rc;
 
 	(*ndc)->state=NDC_ACCEPTING;
