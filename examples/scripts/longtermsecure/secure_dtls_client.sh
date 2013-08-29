edc49f37daaf9cc1a376baefff968e12f81311f1
Oleg Moskalenko <mom040267@gmail.com>
2013-08-29 07:33:36 +0000

    working on DTLS

--- examples/scripts/longtermsecure/secure_dtls_client.sh
+++ examples/scripts/longtermsecure/secure_dtls_client.sh
@@ -14,7 +14,7 @@
 # 6) -m 10 means that 10 clients are emulated.
 # 7) -l 170 means that the payload size of the packets is 170 bytes 
 # (like average audio RTP packet).
-# 8) -e ::1 means that the clients will use peer IPv6 address ::1.
+# 8) -e 127.0.0.1 means that the clients will use peer IPv4 address 127.0.0.1.
 # 9) -g means "set DONT_FRAGMENT parameter in TURN requests".
 # 10) -u ninefingers means that if the server challenges the client with 
 # authentication challenge, then we use account "ninefingers".
@@ -31,5 +31,5 @@ fi
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib/
 
-PATH=examples/bin/:../bin:./bin/:${PATH} turnutils_uclient -S -i turn_client_cert.pem -k turn_client_pkey.pem -n 1000 -m 10 -l 170 -e ::1 -g -u ninefingers -w youhavetoberealistic -s $@ 127.0.0.1
+PATH=examples/bin/:../bin:./bin/:${PATH} turnutils_uclient -S -i turn_client_cert.pem -k turn_client_pkey.pem -n 1000 -m 10 -l 170 -e 127.0.0.1 -g -u ninefingers -w youhavetoberealistic -s $@ 127.0.0.1
 
