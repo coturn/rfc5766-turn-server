7a1df44f008bbc33feb6ba59b9bc77d102982e45
Oleg Moskalenko <mom040267@gmail.com>
2013-10-15 06:43:58 +0000

    version updated

--- src/ns_turn_defs.h
+++ src/ns_turn_defs.h
@@ -31,7 +31,7 @@
 #ifndef __IOADEFS__
 #define __IOADEFS__
 
-#define TURN_SERVER_VERSION "2.6.5.2"
+#define TURN_SERVER_VERSION "2.6.5.3"
 #define TURN_SERVER_VERSION_NAME "Harding Grim"
 #define TURN_SOFTWARE "Citrix-"TURN_SERVER_VERSION" '"TURN_SERVER_VERSION_NAME"'"
 
