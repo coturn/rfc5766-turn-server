a9be01726f2f176e52bd2af7453b1e1eb7853db0
Oleg Moskalenko <mom040267@gmail.com>
2013-11-02 19:23:31 +0000

    cleaning

--- rpm/CentOS6.build.sh
+++ rpm/CentOS6.build.sh
@@ -4,69 +4,48 @@ TURNVERSION=2.6.6.2
 
 BUILDDIR=~/rpmbuild
 ARCH=`uname -i`
-LIBEVENTVERSION=2.0.21
+LIBEVENT2VERSION=2.0.21
+LIBEVENT2DISTRO=libevent-${LIBEVENT2VERSION}-stable.tar.gz
+EPELRPM=epel-release-6-8.noarch.rpm
+
+# DIRS
+
+mkdir -p ${BUILDDIR}
+mkdir -p ${BUILDDIR}/SOURCES
+mkdir -p ${BUILDDIR}/SPECS
+mkdir -p ${BUILDDIR}/RPMS
+mkdir -p ${BUILDDIR}/tmp
+
+# Common packs
+
+for PACK in "make gcc redhat-rpm-config rpm-build doxygen openssl-devel wget svn"
+do
+    sudo yum update ${PACK}
+    ER=$?
+    if ! [ ${ER} -eq 0 ] ; then
+	echo "Cannot install package ${PACK}"
+	exit -1
+    fi
+done
 
 # Libevent2:
 
-sudo yum install make
-ER=$?
-if ! [ ${ER} -eq 0 ] ; then
-    exit -1
-fi
-
-sudo yum install gcc
-ER=$?
-if ! [ ${ER} -eq 0 ] ; then
-    exit -1
-fi
-
-sudo yum install redhat-rpm-config
-ER=$?
-if ! [ ${ER} -eq 0 ] ; then
-    exit -1
-fi
-
-sudo yum install rpm-build
-ER=$?
-if ! [ ${ER} -eq 0 ] ; then
-    exit -1
-fi
-
-sudo yum install doxygen
-ER=$?
-if ! [ ${ER} -eq 0 ] ; then
-    exit -1
-fi
-
-sudo yum install openssl-devel
-ER=$?
-if ! [ ${ER} -eq 0 ] ; then
-    exit -1
-fi
-
-sudo yum install wget
-ER=$?
-if ! [ ${ER} -eq 0 ] ; then
-    exit -1
-fi
-
-rm -rf ${BUILDDIR}
-mkdir ${BUILDDIR}
-mkdir ${BUILDDIR}/SOURCES
-mkdir ${BUILDDIR}/SPECS
-
 cd ${BUILDDIR}/SOURCES
-wget https://github.com/downloads/libevent/libevent/libevent-${LIBEVENTVERSION}-stable.tar.gz
-ER=$?
-if ! [ ${ER} -eq 0 ] ; then
-    exit -1
+if ! [ -f  ${LIBEVENT2DISTRO} ] ; then
+    wget https://github.com/downloads/libevent/libevent/${LIBEVENT2DISTRO}
+    ER=$?
+    if ! [ ${ER} -eq 0 ] ; then
+	exit -1
+    fi
 fi
 
 cd ${BUILDDIR}/SPECS
-wget https://raw.github.com/crocodilertc/libevent/master/libevent.spec
-ER=$?
-if ! [ ${ER} -eq 0 ] ; then
-    exit -1
+if ! [ -f libevent.spec ] ; then
+    wget https://raw.github.com/crocodilertc/libevent/master/libevent.spec
+    ER=$?
+    if ! [ ${ER} -eq 0 ] ; then
+	exit -1
+    fi
 fi
 
 rpmbuild -ba ${BUILDDIR}/SPECS/libevent.spec
@@ -75,35 +54,39 @@ if ! [ ${ER} -eq 0 ] ; then
     exit -1
 fi
 
+for PACK in "${BUILDDIR}/RPMS/${ARCH}/libevent-${LIBEVENT2VERSION}-1.el6.${ARCH}.rpm ${BUILDDIR}/RPMS/${ARCH}/libevent-devel-${LIBEVENT2VERSION}-1.el6.${ARCH}.rpm"
+do
+    sudo yum --skip-broken update ${PACK}
+    ER=$?
+    if ! [ ${ER} -eq 0 ] ; then
+	echo "Cannot install package ${PACK}"
+	exit -1
+    fi
+done
 
 # TURN
 
-sudo rpm -i ${BUILDDIR}/RPMS/${ARCH}/libevent-${LIBEVENTVERSION}-1.el6.${ARCH}.rpm
-
-sudo rpm -i ${BUILDDIR}/RPMS/${ARCH}/libevent-devel-${LIBEVENTVERSION}-1.el6.${ARCH}.rpm
-
-sudo rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
-
-sudo yum install mysql-devel
-ER=$?
-if ! [ ${ER} -eq 0 ] ; then
-    exit -1
+cd ${BUILDDIR}/RPMS
+if ! [ -f ${EPELRPM} ] ; then
+    wget http://download.fedoraproject.org/pub/epel/6/i386/${EPELRPM}
+    ER=$?
+    if ! [ ${ER} -eq 0 ] ; then
+	exit -1
+    fi
 fi
 
-sudo yum install postgresql-devel
-ER=$?
-if ! [ ${ER} -eq 0 ] ; then
-    exit -1
-fi
+for PACK in "epel-release-6-8.noarch.rpm mysql-devel postgresql-devel hiredis-devel"
+do
+    sudo yum update ${PACK}
+    ER=$?
+    if ! [ ${ER} -eq 0 ] ; then
+	echo "Cannot install package ${PACK}"
+	exit -1
+    fi
+done
 
-sudo yum install hiredis-devel
-ER=$?
-if ! [ ${ER} -eq 0 ] ; then
-    exit -1
-fi
-
-mkdir ${BUILDDIR}/tmp
 cd ${BUILDDIR}/tmp
+rm -rf turnserver-${TURNVERSION}
 svn export http://rfc5766-turn-server.googlecode.com/svn/trunk/ turnserver-${TURNVERSION}
 ER=$?
 if ! [ ${ER} -eq 0 ] ; then
