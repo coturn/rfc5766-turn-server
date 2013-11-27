ad4095d2011700a287eaca7933127f493ec8814c
Oleg Moskalenko <mom040267@gmail.com>
2013-11-27 04:26:57 +0000

    negative protocol tests improvements

--- src/apps/uclient/uclient.c
+++ src/apps/uclient/uclient.c
@@ -192,9 +192,12 @@ int send_buffer(app_ur_conn_info *clnet_info, stun_buffer* message, int data_con
 
 	if(negative_protocol_test && (message->len>0)) {
 		if(random()%10 == 0) {
-			int pos = (int)((unsigned long)random()%(unsigned long)message->len);
-			int val = (int)((unsigned long)random()%256);
-			message->buf[pos]=(u08bits)val;
+			int np = (int)((unsigned long)random()%10);
+			while(np-->0) {
+				int pos = (int)((unsigned long)random()%(unsigned long)message->len);
+				int val = (int)((unsigned long)random()%256);
+				message->buf[pos]=(u08bits)val;
+			}
 		}
 	}
 
