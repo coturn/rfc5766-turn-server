7c2d48323be09f01dcb5e453d9f174d067801cb0
Oleg Moskalenko <mom040267@gmail.com>
2013-11-28 08:14:19 +0000

    uninstall fixed

--- rpm/uninstall.turnserver.sh
+++ rpm/uninstall.turnserver.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-for i in `rpm -q -a | grep turnserver-utils-2`
+for i in `rpm -q -a | grep turnserver-utils-3`
 do
   echo $i
   sudo rpm -e $i
 done
 
-for i in `rpm -q -a | grep turnserver-client-libs-2`
+for i in `rpm -q -a | grep turnserver-client-libs-3`
 do
   echo $i
   sudo rpm -e $i
 done
 
-for i in `rpm -q -a | grep turnserver.*-2`
+for i in `rpm -q -a | grep turnserver.*-3`
 do
   echo $i
   sudo rpm -e $i
