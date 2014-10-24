efb1f0b3fd1e09e4cd5b62aad71f3cb0c7c2d7c0
Oleg Moskalenko <mom040267@gmail.com>
2014-10-24 23:33:35 +0000

    socket detach fixed

--- src/server/ns_turn_server.c
+++ src/server/ns_turn_server.c
@@ -1278,7 +1278,7 @@ static int handle_turn_refresh(turn_turnserver *server,
 				if(tsid != server->id) {
 
 					if(server->send_socket_to_relay) {
-						ioa_socket_handle new_s = detach_ioa_socket(ss->client_session.s,1);
+						ioa_socket_handle new_s = detach_ioa_socket(ss->client_session.s);
 						if(new_s) {
 						  if(server->send_socket_to_relay(tsid, mid, tid, new_s, message_integrity, 
 										  RMT_MOBILE_SOCKET, in_buffer, can_resume)<0) {
@@ -1358,7 +1358,7 @@ static int handle_turn_refresh(turn_turnserver *server,
 
 								//Transfer socket:
 
-								ioa_socket_handle s = detach_ioa_socket(ss->client_session.s,0);
+								ioa_socket_handle s = detach_ioa_socket(ss->client_session.s);
 
 								ss->to_be_closed = 1;
 
@@ -2024,7 +2024,7 @@ static int handle_turn_connection_bind(turn_turnserver *server,
 				turnserver_id sid = (id & 0xFF000000)>>24;
 				ioa_socket_handle s = ss->client_session.s;
 				if(s && !ioa_socket_tobeclosed(s)) {
-					ioa_socket_handle new_s = detach_ioa_socket(s,1);
+					ioa_socket_handle new_s = detach_ioa_socket(s);
 					if(new_s) {
 					  if(server->send_socket_to_relay(sid, id, tid, new_s, message_integrity, RMT_CB_SOCKET, in_buffer, can_resume)<0) {
 					    *err_code = 400;
