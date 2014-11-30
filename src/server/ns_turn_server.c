5e58b113512f3a251a7b41c0e44cb8c040236e38
Oleg Moskalenko <mom040267@gmail.com>
2014-11-30 18:08:25 +0000

    working on 141

--- src/server/ns_turn_server.c
+++ src/server/ns_turn_server.c
@@ -4210,13 +4210,15 @@ static void peer_input_handler(ioa_socket_handle s, int event_type,
 
 	UNUSED_ARG(can_resume);
 
-	if(ioa_socket_tobeclosed(s)) return;
+	if(!s || ioa_socket_tobeclosed(s)) return;
 
 	ts_ur_super_session* ss = (ts_ur_super_session*) arg;
 
 	if(!ss) return;
 
 	if(ss->to_be_closed) return;
+
+	if(!(ss->client_session.s) || ioa_socket_tobeclosed(ss->client_session.s)) return;
 
 	turn_turnserver *server = (turn_turnserver*) (ss->server);
 
